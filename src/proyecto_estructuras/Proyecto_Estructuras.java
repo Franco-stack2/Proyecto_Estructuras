613f8cdc00e25295f499c50dfc651d0170800fc7
Kerryhub16 <kalfaro40767@ufide.ac.cr>
2026-09-25 16:17:44 -0600

    kerry

--- src/proyecto_estructuras/Proyecto_Estructuras.java
+++ src/proyecto_estructuras/Proyecto_Estructuras.java
@@ -14,9 +14,7 @@ public class Proyecto_Estructuras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //prueba de funcionalidad kerry alfaro
-        // Hola soy francohjhj
+
     }
     
 }
