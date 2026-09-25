ddb18948f4e5c46c0992e20d4a08db50e8114f1b
Franco <14francosmm@gmail.com>
2026-09-25 15:42:14 -0600

    Version inicial del proyecto

--- src/proyecto_estructuras/Proyecto_Estructuras.java
+++ src/proyecto_estructuras/Proyecto_Estructuras.java
@@ -16,7 +16,7 @@ public class Proyecto_Estructuras {
     public static void main(String[] args) {
         // TODO code application logic here
         //prueba de funcionalidad kerry alfaro
-        // Hola soy franco
+        // Hola soy francohjhj
     }
     
 }
