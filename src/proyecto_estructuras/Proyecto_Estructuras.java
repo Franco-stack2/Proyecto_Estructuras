deb29d06322de920da282c67e1faa4e90ba04c6a
jorge <jorge@Laptop-TITO>
2026-09-26 08:29:52 -0600

    "Comentario de prueba 2"

--- src/proyecto_estructuras/Proyecto_Estructuras.java
+++ src/proyecto_estructuras/Proyecto_Estructuras.java
@@ -18,7 +18,8 @@ public class Proyecto_Estructuras {
         //Daniel Mora
         //Daniel 2
         // Probando conexion git TITO
-        //Hola a todos  
+        //Hola a todos
+        // Probando por segunda vez
     }
     
 }
